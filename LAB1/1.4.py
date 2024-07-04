6693bed95e5a0ead409a9e88dbb4fc4e6c956eb5
สารินทร์ อินต๊ะรักษา <118350574+Sarin2545@users.noreply.github.com>
2024-07-04 13:17:18 +0700

    Add files via upload

---
+++ LAB1/1.4.py
@@ -0,0 +1,17 @@
+# List initialization
+number = [1, 2, 3, 4, 5]
+
+# Accessing elements
+print("First element:", number[0]) # Output: 1
+print("Last element:", number[-1]) # Output: 5
+
+# Slicing
+print("Sliced element:", number[2:4]) # Output: [3, 4]
+
+# Appending and extending
+number.append(6)
+print("After append:", number) # Output: [1, 2, 3, 4, 5, 6]
+
+# Removing elements
+number.remove(3)
+print("After removal:", number) # Output: [1, 2, 4, 5, 6]
