21829025798726cb7843230104c9ea8f25b7dd25
สารินทร์ อินต๊ะรักษา <118350574+Sarin2545@users.noreply.github.com>
2024-07-04 13:18:55 +0700

    Delete 1.6.py

--- 1.6.py
+++
@@ -1,7 +0,0 @@
-names = ["Alice", "Bob", "Charlie"]
-ages = [25, 30, 35]
-city = ["New York", "Los Angeles", "chicago"]
-
-# Using zip() for parallel iteration
-for name, age, city in zip(names, ages, city):
-    print(f"{name} is {age} year old and lives in {city}")
